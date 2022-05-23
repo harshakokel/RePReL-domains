d0c608cd93887a8fc83d65fa919bcf0174dcbef0
Nikhilesh <prabhkar.nikhilesh@gmail.com>
2022-05-23 00:20:56 -0500

    Taxi Domain
    Changes to Init

--- taxi_domain/__init__.py
+++ taxi_domain/__init__.py
@@ -25,6 +25,8 @@ register(
 )
 
 
+
+
 register(
     id='RelationalTaxiWorld-graph-task1-v1',
     entry_point='taxi_domain.envs:RelationalTaxiWorld',
@@ -47,5 +49,48 @@ register(
             'passenger_count': 3, "max_passenger": 3}
 )
 
+register(
+    id='RelationalTaxiWorld-graph-task4-v1',
+    entry_point='taxi_domain.envs:RelationalTaxiWorld',
+    kwargs={'obs_type': "graph", 'layout': eight_layout,
+            'passenger_count': 5, "max_passenger": 5}
+)
+
+register(
+    id='RelationalTaxiWorld-task1-v2',
+    entry_point='taxi_domain.envs:RelationalTaxiWorld',
+    kwargs={'obs_type': "vec", 'layout': eight_layout,
+            'passenger_count': 1, "max_passenger": 3}
+)
+
+
+register(
+    id='RelationalTaxiWorld-task2-v2',
+    entry_point='taxi_domain.envs:RelationalTaxiWorld',
+    kwargs={'obs_type': "vec", 'layout': eight_layout,
+            'passenger_count': 2, "max_passenger": 3}
+)
+
+
+register(
+    id='RelationalTaxiWorld-task3-v2',
+    entry_point='taxi_domain.envs:RelationalTaxiWorld',
+    kwargs={'obs_type': "vec", 'layout': eight_layout,
+            'passenger_count': 3, "max_passenger": 3}
+)
+
+register(
+    id='RelationalTaxiWorld-task1-v3',
+    entry_point='taxi_domain.envs:RelationalTaxiWorld',
+    kwargs={'obs_type': "vec", 'layout': five_by_five_layout, "max_passenger": 2,
+            'passenger_count': 1}
+)
+
+register(
+    id='RelationalTaxiWorld-task1-v4',
+    entry_point='taxi_domain.envs:RelationalTaxiWorld',
+    kwargs={'obs_type': "dict", 'layout': five_by_five_layout, "max_passenger": 2,
+            'passenger_count': 1}
+)
 
 
